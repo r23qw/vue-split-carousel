57676b256624b41e57dd39161020e196732b0e8d
aaron00101010 <aaron00101010@gmail.com>
2021-04-07 10:50:30 +0800

    chore: build assets

--- scripts/webpack.config.esm.js
+++ scripts/webpack.config.esm.js
@@ -1,15 +1,13 @@
 const config = require("./webpack.config.base");
 const merge = require("webpack-merge");
 const path = require("path");
-
+const EsmWebpackPlugin =  require("@purtuga/esm-webpack-plugin")
 const prodConfig = merge(config, {
   mode: "development",
   output: {
     path: path.resolve(__dirname, "../dist"),
-    filename: "vue-split-carousel.esm.js"
+    filename: "vue-split-carousel.esm.js",
   },
-  optimization: {
-    usedExports: true
-  }
+  plugins: [new EsmWebpackPlugin()],
 });
 module.exports = prodConfig;
